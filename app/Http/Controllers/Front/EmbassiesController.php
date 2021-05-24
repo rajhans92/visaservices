5ba5b66e522c40429efb8f4d858954d2563f4abb
rajhans92 <rupesh.rajhans92@gmail.com>
2021-05-24 15:48:28 +0530

    update embassy backend process

--- app/Http/Controllers/Front/EmbassiesController.php
+++ app/Http/Controllers/Front/EmbassiesController.php
@@ -48,7 +48,41 @@ class EmbassiesController extends Controller
       $data = DB::table('embassies')->get();
       $embassyList = DB::table('embassies_detail')->where('embassies_id',$embassy->id)->get();
 
-      return view('front.embassies.detail',compact('embassyList','data','embassy'));
+
+      $secondDropdown = DB::table('country')
+      ->select(
+          'country.id as id',
+          'country.country_name as country_name',
+          'country.country_code as country_code',
+          'route_visa.visa_url as visa_url'
+        )
+      ->where('country.language_id',env('APP_LANG'))
+      ->where('route_visa.type_of_url',"visa")
+      ->whereIn('country.country_name',function($db){
+        $db->select('visa_pages.country_name')
+            ->from('visa_pages')
+            ->where('visa_pages.language_id',env('APP_LANG'))
+            ->where('route_visa.type_of_url',"visa")
+            ->join('route_visa','route_visa.visa_id',"=","visa_pages.id");
+      })
+      ->join('visa_pages','visa_pages.country_name',"=","country.country_name")
+      ->join('route_visa','route_visa.visa_id',"=","visa_pages.id")
+      ->get();
+
+      $homeData = DB::table('home_page')
+      ->join('home_client_review','home_client_review.language_id','=','home_page.language_id')
+      ->join('home_section_2','home_section_2.language_id','=','home_page.language_id')
+      ->join('home_section_3','home_section_3.language_id','=','home_page.language_id')
+      ->join('home_info_section','home_info_section.language_id','=','home_page.language_id')
+      ->where('home_page.language_id',env('APP_LANG'))
+      ->first();
+
+      $footerData = DB::table('footer_detail')
+      ->where('footer_detail.language_id',env('APP_LANG'))
+      ->first();
+
+
+      return view('front.embassies.detail',compact('embassyList','data','embassy','secondDropdown','homeData','footerData'));
 
     }
 
--- resources/views/front/embassies/detail.blade.php
+++ resources/views/front/embassies/detail.blade.php
@@ -22,6 +22,7 @@
           <div class="in-text">In</div>
           <div class="select-country-dropdown">
               <select id="embassy_in">
+                <option value="">Select Country</option>
                 @foreach($embassyList as $key => $val)
                   <option value="{{$val->embassy_in}}">{{$val->embassy_in}}</option>
                 @endforeach
@@ -73,14 +74,14 @@
              <form>
                <div class="form-group">
                  <label for="locationto" class="form-label">Where am I Going?</label>
-                 <select class="form-select" aria-label="Where am I Going?">
-                  <option selected>INDIA (IN)</option>
-                  <option value="1">EGYPT (EG)</option>
-                  <option value="2">MEXICO (MX)</option>
+                 <select class="form-select" aria-label="Where am I Going?" id="secondDropdown">
+                   @foreach($secondDropdown as $key => $val)
+                     <option value="{{$val->visa_url}}" data="{{$val->country_name}}">{{$val->country_name}} ({{$val->country_code}})</option>
+                   @endforeach
                 </select>
                </div>
                <div class="form-group">
-                  <button type="button" class="btn apply-btn">Apply</button>
+                  <button type="button" class="btn apply-btn" id="applyBtn">Apply</button>
                </div>
              </form>
            </div>
@@ -93,10 +94,9 @@
           </div>
           <div class="faq-social-links">
               <ul>
-                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                  <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-                  <li><a href="#"><i class="fab fa-pinterest-p"></i></a></li>
+                  <li><a href="{{$footerData->social_network_link1}}"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="{{$footerData->social_network_link2}}"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="{{$footerData->social_network_link3}}"><i class="fab fa-instagram"></i></a></li>
               </ul>
           </div>
       </div>
@@ -108,27 +108,27 @@
 <!-- REVIEWS -->
 <section class="reviews-section section-spacing">
    <div class="container">
-     <h2 class="section-head text-center">Our Customers Have Great Stories About Us</h2>
+     <h2 class="section-head text-center">{{$homeData->section_4_title}}</h2>
      <div class="row">
        <div class="col-lg-6 col-md-6 col-sm-12">
          <div class="review-box">
            <div class="img">
-             <img src="https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg" />
+             <img src="{{url('images/home/'.$homeData->client_img_1) }}" onerror="this.src='{{ url('images/default.png') }}'" />
            </div>
            <div class="info">
-              <h4>Alan</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+              <h4>{{$homeData->client_name_1}}</h4>
+              <p>{{$homeData->client_review_1}}</p>
            </div>
        </div>
       </div>
       <div class="col-lg-6 col-md-6 col-sm-12">
          <div class="review-box">
            <div class="img">
-             <img src="https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg" />
+             <img src="{{url('images/home/'.$homeData->client_img_2) }}" onerror="this.src='{{ url('images/default.png') }}'"  />
            </div>
            <div class="info">
-              <h4>Alan</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+             <h4>{{$homeData->client_name_2}}</h4>
+             <p>{{$homeData->client_review_2}}</p>
            </div>
        </div>
       </div>
@@ -137,33 +137,51 @@
        <div class="col-lg-6 col-md-6 col-sm-12">
          <div class="review-box">
            <div class="img">
-             <img src="https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg" />
+             <img src="{{url('images/home/'.$homeData->client_img_3) }}" onerror="this.src='{{ url('images/default.png') }}'"  />
            </div>
            <div class="info">
-              <h4>Alan</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+             <h4>{{$homeData->client_name_3}}</h4>
+             <p>{{$homeData->client_review_3}}</p>
            </div>
        </div>
       </div>
       <div class="col-lg-6 col-md-6 col-sm-12">
          <div class="review-box">
            <div class="img">
-             <img src="https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg" />
+             <img src="{{url('images/home/'.$homeData->client_img_4) }}" onerror="this.src='{{ url('images/default.png') }}'"  />
            </div>
            <div class="info">
-              <h4>Alan</h4>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+             <h4>{{$homeData->client_name_4}}</h4>
+             <p>{{$homeData->client_review_4}}</p>
            </div>
        </div>
       </div>
     </div>
    </div>
 </section>
-
 @stop
 @section('javascript')
 
 <script type="text/javascript">
+$(document).ready(function(){
+  $("#applyBtn").click(function(){
+      let url = $("#secondDropdown").val();
 
+      var d = new Date();
+      d.setTime(d.getTime() + (2*24*60*60*1000));
+      var expires = "expires="+ d.toUTCString();
+
+      let goname = "go_country";
+      let fromname = "from_country";
+      let govalue = $("#secondDropdown option").filter(":selected").attr("data");
+      let fromvalue = '{{strtolower($embassy->name)}}';
+
+      document.cookie = fromname + "=" + fromvalue + ";" + expires + ";path=/";
+      document.cookie = goname + "=" + govalue + ";" + expires + ";path=/";
+      if(url.length > 0)
+        window.location = window.location.origin + '/'+ url;
+
+  });
+});
 </script>
 @endsection
